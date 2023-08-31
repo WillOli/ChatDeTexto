a7c1be01181a7278bfbc22204c797f1ba7e36301
WillOli <william.ika@gmail.com>
2023-08-31 11:57:20 -0300

    first commit

---
+++ src/main/java/com/example/ChatDeTexto/ChatDeTextoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ChatDeTexto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatDeTextoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatDeTextoApplication.class, args);
+	}
+
+}
